4c0c8433faf0f32cee85214adfd93f8e3889eb9b
DESKTOP-U06KDH6\admin <longlongvip@outlook.com>
2024-04-13 20:33:34 +0800

    初始提交

---
+++ A/A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <numeric>
+#include <sstream>
+#include <algorithm>
+
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
